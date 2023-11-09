904845e2f3f8f0c70725fa67bc82ae33b0cdc29c
yoxenl <caisaskog@gmail.com>
2023-11-09 14:27:45 +0100

    simple motor script

---
+++ giopzero.py
@@ -0,0 +1,12 @@
+from giopzero import Motor, PhaseEnableMotor
+from time import sleep
+
+motor = Motor(17, 18)
+motor2 = PhaseEnableMotor(12, 5)
+motor.forward()
+sleep(5)
+motor.stop()
+sleep(2)
+motor2.forward()
+sleep(5)
+motor2.stop()
